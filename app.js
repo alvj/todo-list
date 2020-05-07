ee20207c2fcacab9d70a3544b1995b9cf921e8c3
Alvaro <alvarojperezperez@gmail.com>
2020-05-07 20:26:12 +0200

    Added functionality for saving todo state

--- app.js
+++ app.js
@@ -25,10 +25,13 @@ function addTodoBtn(event){
     todoInput.value = "";
 }
 
-function addTodo(todo){
+function addTodo(todo, checked=false){
     //Todo div
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
+        if(checked){
+            todoDiv.classList.add('completed')
+        }
 
     //Create li
     const newTodo = document.createElement('li');
@@ -55,18 +58,20 @@ function addTodo(todo){
 function manageTodos(event) {
     const item = event.target;
     const todo = item.parentElement;
+    const todoValue = todo.children[0].innerText
 
     //Delete TO DO
     if (item.classList[0] === 'trash-btn'){
         //Animation
         todo.classList.add('fall');
-        removeLocalTodos(todo);
+        removeLocalTodos(todoValue);
         todo.addEventListener('transitionend', () => todo.remove())
     }
 
     //Check TO DO
     if(item.classList[0] === 'complete-btn'){
-        todo.classList.toggle('completed');
+        checked = todo.classList.toggle('completed');
+        saveLocalTodos(todoValue, checked);
     }
 }
 
@@ -105,15 +110,26 @@ function getLocalTodos(){
     return todos;
 }
 
-function saveLocalTodos(todo){
+function saveLocalTodos(todoValue, checked=false){
     let todos = getLocalTodos();
-    todos.push(todo);
+    //Find the index of the todo
+    todoIndex = findIndex(todos, "value", todoValue);
+
+    if(todos[todoIndex] === undefined){
+        //If that todo is not defined in local storage yet, then create it and push it into local storage
+        let todo = {value:todoValue, checked:checked}
+        todos.push(todo);
+    }else{
+        //If that todo is in local storage, update its checked property
+        todos[todoIndex].checked = checked;
+    }
+    //Save todos to local storage
     localStorage.setItem('todos', JSON.stringify(todos));
 }
 
-function removeLocalTodos(todo){
+function removeLocalTodos(todoValue){
     let todos = getLocalTodos();
-    const todoIndex = todos.indexOf(todo.children[0].innerText);
+    const todoIndex = findIndex(todos, "value", todoValue);
     todos.splice(todoIndex, 1);
     localStorage.setItem('todos', JSON.stringify(todos));
 }
@@ -121,6 +137,16 @@ function removeLocalTodos(todo){
 function showLocalTodos(){
     let todos = getLocalTodos();
     todos.forEach(function(todo){
-        addTodo(todo);
+        addTodo(todo.value, todo.checked);
     })
+}
+
+//Loop through and array of objects and return the index of the object with value value in attribute attr
+function findIndex(array, attr, value){
+    for (let i = 0; i < array.length; i++) {
+        if(array[i][attr] === value){
+            return i;
+        }
+    }
+    return -1;
 }
